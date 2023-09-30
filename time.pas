aaabf439ccf8bdcb506231880c7511a0dfdd477d
Fuuzy <iqorbondar374913@gmail.com>
2023-09-30 10:33:23 +0300

    Программа проверки на год

---
+++ time.pas
@@ -0,0 +1,8 @@
+﻿begin
+    var yer := readinteger('год :');
+
+    if (yer mod 4 = 0) and ((yer mod 100 <> 0) or (yer mod 400 = 0)) then 
+        print('Год високосный')
+    else 
+        print('Год не високосный');
+end.
