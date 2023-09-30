219f39fe63eb1d85de9803dc5469c416d89cf1a2
Fuuzy <iqorbondar374913@gmail.com>
2023-09-30 10:27:15 +0300

    Ваше сообщение

---
+++ hello.pas
@@ -0,0 +1,10 @@
+﻿begin
+  var a := readinteger('Время :');
+  assert(a in 0..23);
+  case a of 
+    4..10 : print('Доброе утро, мир!');
+    11..16 : print('Добрый день, мир!');
+    17..22 : print('Добрый ночи, мир!');
+  else print('Доброй ночи, мир!');
+  end;
+end.
